2ce265be5a9af5822d9c5ef9fe5813e152c0d2cb
Ashley Schauer <ashley.schauer@givecampus.com>
2023-07-24 22:08:48 -0600

    add select all checkbox toggle

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,13 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "name" ]
+  static targets = [ "checkbox", "toggleCheckbox" ]
 
-  greet() {
-    console.log(`Hello, ${this.name}!`)
+  toggle_option_checkbox({ srcElement }) {
+    var allChecked = this.checkboxTargets.every((checkboxTarget) => checkboxTarget.checked) 
+    this.toggleCheckboxTarget.checked = allChecked
   }
 
-  get name() {
-    return this.nameTarget.value
+  toggle_select_all_checkbox({ srcElement }) {
+    var is_checked = this.toggleCheckboxTarget.checked
+    this.checkboxTargets.forEach((checkboxTarget) => {
+      checkboxTarget.checked = is_checked
+    })
   }
 }
