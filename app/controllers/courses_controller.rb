116bfc38e5e5b16bd78df7bc71480eeda2744799
Ashley Schauer <ashley.schauer@givecampus.com>
2023-07-24 22:55:15 -0600

    cleanup url

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,6 @@
 class CoursesController < ApplicationController
   def index
+    clean_params
     load_courses
     filter_courses
     sort_courses
@@ -9,20 +10,18 @@ class CoursesController < ApplicationController
   private
 
   def load_courses
-    @courses = Course
-      .includes(:department, department: :school)
-      .all
+    @courses = Course.includes(:department, department: :school).all
   end
 
   def filter_courses
-    params.dig(:filter).try(:permit!)
+    params[:filter].try(:permit!)
 
     filter_by_credits if params.dig(:filter, :credits).present?
     filter_by_department if params.dig(:filter, :department).present?
   end
 
   def filter_by_credits
-    @courses = @courses.where(credits: params[:filter][:credits].keys)
+    @courses = @courses.where(credits: params[:filter][:credits])
   end
 
   def filter_by_department
@@ -30,10 +29,16 @@ class CoursesController < ApplicationController
   end
 
   def sort_courses
-    @courses = @courses.order("schools.name ASC", "departments.code ASC", "course_number ASC")
+    @courses = @courses.order('schools.name ASC', 'departments.code ASC', 'course_number ASC')
   end
 
   def paginate_courses
     @courses = Kaminari.paginate_array(@courses).page(params[:page]).per(25)
+  end
+
+  def clean_params
+    if params.dig(:filter, :credits).present?
+      params[:filter][:credits] = params[:filter][:credits].keys
+    end
   end
 end
